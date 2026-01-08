09342ac0aee26c4b7264b400d3433129ecd74e23
sofiaprata <sofiaprata9@gmail.com>
2026-01-07 22:18:00 -0300

    sv

--- script.js
+++ script.js
@@ -3,19 +3,26 @@ const btnNavContato = document.getElementById("btnNavContato");
 const modal = document.getElementById("modalContato");
 const closeModal = document.getElementById("closeModal");
 
-function openModal() {
+function openModal(e) {
+  if (e) e.preventDefault();   // 🔴 ISSO É O QUE FALTAVA
   modal.style.display = "flex";
 }
 
+// botão do hero
 btnContato.addEventListener("click", openModal);
+
+// botão da nav
 btnNavContato.addEventListener("click", openModal);
 
+// fechar no X
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+// fechar clicando fora
 modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
 });
+
